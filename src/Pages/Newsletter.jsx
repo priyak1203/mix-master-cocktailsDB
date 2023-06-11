e10e2cd80dc465ded2359ed7f56ed47a57c83c75
Priydarshini Karanth <kpriya1203@gmail.com>
2023-06-11 21:00:12 +0530

    form submission with react router form and action

--- src/Pages/Newsletter.jsx
+++ src/Pages/Newsletter.jsx
@@ -1,5 +1,66 @@
+import axios from 'axios';
+import { Form, redirect } from 'react-router-dom';
+import { toast } from 'react-toastify';
+
+const newsletterUrl = 'https://www.course-api.com/cocktails-newsletter';
+
+export const action = async ({ request }) => {
+  const formData = await request.formData();
+  const data = Object.fromEntries(formData);
+
+  const response = await axios.post(newsletterUrl, data);
+  toast.success(response.data.msg);
+  return redirect('/');
+};
+
 const Newsletter = () => {
-  return <div>Newsletter</div>;
+  return (
+    <>
+      <Form className="form" method="POST">
+        <h4>our newsletter</h4>
+        {/* name */}
+        <div className="form-row">
+          <label htmlFor="name" className="form-label">
+            name
+          </label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            className="form-input"
+            defaultValue="john"
+          />
+        </div>
+        {/* last name */}
+        <div className="form-row">
+          <label htmlFor="lastName" className="form-label">
+            last name
+          </label>
+          <input
+            type="text"
+            name="lastName"
+            id="lastName"
+            className="form-input"
+            defaultValue="smith"
+          />
+        </div>
+        <div className="form-row">
+          <label htmlFor="email" className="form-label">
+            email
+          </label>
+          <input
+            type="text"
+            name="email"
+            id="email"
+            className="form-input"
+            defaultValue="test@test.com"
+          />
+        </div>
+
+        <button className="btn btn-block">submit</button>
+      </Form>
+    </>
+  );
 };
 
 export default Newsletter;
