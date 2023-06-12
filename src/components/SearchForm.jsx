b2efbe85d665bd8ace283eb9a0ca00470b437cd3
Priydarshini Karanth <kpriya1203@gmail.com>
2023-06-13 01:06:16 +0530

    search form and search functionality

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,5 +1,41 @@
-const SearchForm = () => {
-  return <div>SearchForm</div>;
+import { Form } from 'react-router-dom';
+import styled from 'styled-components';
+
+const SearchForm = ({ SearchTerm }) => {
+  return (
+    <Wrapper>
+      <Form className="form">
+        <input
+          type="search"
+          className="form-input"
+          name="search"
+          defaultValue={SearchTerm}
+        />
+        <button type="submit" className="btn">
+          search
+        </button>
+      </Form>
+    </Wrapper>
+  );
 };
 
 export default SearchForm;
+
+const Wrapper = styled.section`
+  margin-bottom: 6rem;
+  .form {
+    display: grid;
+    grid-template-columns: 1fr auto;
+  }
+
+  .form-input {
+    border-top-right-radius: 0;
+    border-bottom-right-radius: 0;
+  }
+
+  .form .btn {
+    border-top-left-radius: 0;
+    border-bottom-left-radius: 0;
+    margin-top: 0;
+  }
+`;
